0a8842126949560c5a24a936a753015b84d196ac
Angieleo <gutierrezhangie2a@gmail.com>
2021-06-07 22:01:44 -0500

    operadores matematicos

---
+++ python/ejercicio008.py
@@ -0,0 +1,21 @@
+# STRING y más
+
+esto_es_una_string="hola angie gutierrez"
+esto_es_otra_string="mundo"
+
+#concatenar 
+print(esto_es_una_string + ' ' + esto_es_otra_string) #Hola Mundo
+
+# MAYUSCULA
+print(esto_es_una_string.upper())
+# minuscula
+print(esto_es_una_string.lower())
+# primera mayuscula
+print(esto_es_una_string.capitalize())
+# Mayuscula La Primera Letra De Cada Palabra
+print(esto_es_una_string.title())
+# longitud
+print(len(esto_es_una_string))
+
+
+
