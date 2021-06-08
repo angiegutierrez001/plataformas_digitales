6a3d381824594701fde63d0212f2b3bf3bfd4e8d
Angieleo <gutierrezhangie2a@gmail.com>
2021-06-07 22:11:31 -0500

    string y mas

--- python/ejercicio008.py
+++ python/ejercicio008.py
@@ -6,16 +6,25 @@ esto_es_otra_string="mundo"
 #concatenar 
 print(esto_es_una_string + ' ' + esto_es_otra_string) #Hola Mundo
 
-# MAYUSCULA
-print(esto_es_una_string.upper())
-# minuscula
-print(esto_es_una_string.lower())
-# primera mayuscula
-print(esto_es_una_string.capitalize())
-# Mayuscula La Primera Letra De Cada Palabra
-print(esto_es_una_string.title())
-# longitud
-print(len(esto_es_una_string))
+
+print(esto_es_una_string.upper()) # MAYUSCULA
+
+print(esto_es_una_string.lower()) # minuscula
+
+print(esto_es_una_string.capitalize()) # primera mayuscula
+
+print(esto_es_una_string.title()) # Mayuscula La Primera Letra De Cada Palabra
+
+print(len(esto_es_una_string)) # longitud
+
+print(esto_es_una_string.find('e')) # buscar una caracter y muestra su ubicacion en indice
+
+print(esto_es_una_string[0:1]) #rebanar las letras
+print(esto_es_una_string[5:10]) #rebanar las letras
+
+print(esto_es_una_string[::-1]) #radar
+
+
 
 
 
