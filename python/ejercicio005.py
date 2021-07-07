c2a93ee7847236e29495922823c9aa7cf2001da1
Angieleo <gutierrezhangie2a@gmail.com>
2021-07-07 09:44:36 -0500

    calcular imc

---
+++ python/ejercicio005.py
@@ -0,0 +1,9 @@
+# pedir tu estatura y peso, calcular el imc
+
+
+estatura = float(input("Por favor ingrese su estatura en metros \n"))
+peso = float(input("Por favor ingrese su peso en kilos \t"))
+
+imc = estatura / peso
+
+print(f"Su imc es igual a {imc}")
