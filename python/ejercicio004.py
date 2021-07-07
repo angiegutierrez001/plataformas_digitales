2b07af1a518eda2a9fdb29349e3d7152547553ca
Angieleo <gutierrezhangie2a@gmail.com>
2021-07-07 09:42:41 -0500

    area del circulo

---
+++ python/ejercicio004.py
@@ -0,0 +1,10 @@
+# area y perimetro del circulo
+
+radio = float(input('por favor ingresa el valor del radio del circulo: '))
+
+PI = 3.1415
+
+perimetro= 2 * PI * radio
+area= PI * radio ** 2
+
+print(f'El valor del perimetro es: {perimetro} y el area es {area}')
---
+++ python/ejercicio010.py
@@ -0,0 +1,11 @@
+#operadores de relación
+
+edad_ayovi = 20
+edad_jose = 39
+
+print(edad_ayovi > edad_jose)  # mayor
+print(edad_ayovi < edad_jose)  # menor
+print(edad_ayovi == edad_jose)  # igualdad
+print(edad_ayovi != edad_jose)  # diferencia, no igual
+print(edad_ayovi >= edad_jose)  # mayor o igual
+print(edad_ayovi <= edad_jose)  # menor o igual
